814e1fb8876a71b230eeed303e65c5c7ede3e8e0
Jermiah Joseph <jermiahjoseph98@gmail.com>
2024-04-19 09:25:29 -0400

    update project ids and buckets

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -15,7 +15,7 @@
 
 terraform {
   backend "gcs" {
-    bucket = "PROJECT_ID-tfstate"
+    bucket = "orcestra-388613-tfstate"
     prefix = "env/dev"
   }
 }
--- environments/dev/terraform.tfvars
+++ environments/dev/terraform.tfvars
@@ -1,1 +1,1 @@
-project="PROJECT_ID"
+project="orcestra-388613"
